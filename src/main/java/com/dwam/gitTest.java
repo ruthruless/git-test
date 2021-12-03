40e74a6ad9e1203bb9afb9cca06209073e1c7cab
dawm <tandawn@foxmain.com>
2021-12-03 14:20:06 +0800

    hot-fix first commit

--- src/main/java/com/dwam/gitTest.java
+++ src/main/java/com/dwam/gitTest.java
@@ -5,5 +5,6 @@ public class gitTest {
         System.out.println("hello git");
         System.out.println("hello git1");
         System.out.println("hello git1");
+        System.out.println("hello git hot-fix");
     }
 }
